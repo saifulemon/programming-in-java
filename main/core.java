dd24a2b0af27a9b59b6f23821d24d82bd46ef48e
saifulemon <463saiful0@gmail.com>
2023-01-15 20:03:58 +0600

    if-else statement

--- main/core.java
+++ main/core.java
@@ -4,3 +4,16 @@ public class Student {
       System.out.println("Hello World");
     }
 }
+
+// write a java program using if else statement
+public class IfElseExample {
+    public static void main (String[] args){
+        int number = 12;
+        if(number>= 10){
+            System.out.println("number: " + number);
+        }
+        else {
+           System.out.println("No Data");
+        }
+    }
+}
