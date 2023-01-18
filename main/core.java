a316e2a82c545a4f67e4b53626a451f7d5e6ce05
saifulemon <463saiful0@gmail.com>
2023-01-18 22:33:22 +0600

    interface example

--- main/core.java
+++ main/core.java
@@ -155,3 +155,22 @@ class XYZ extends ABC {
         obj.methodB();
     }
 }
+
+
+// write a java program using Interface
+interface Drawable{  
+void draw();  
+}  
+//Implementation: by second user  
+class Rectangle implements Drawable{  
+public void draw(){System.out.println("drawing rectangle");}  
+}  
+class Circle implements Drawable{  
+public void draw(){System.out.println("drawing circle");}  
+}  
+//Using interface: by third user  
+class TestInterface1{  
+public static void main(String args[]){  
+Drawable d=new Circle();//In real scenario, object is provided by method e.g. getDrawable()  
+d.draw();  
+}}  
